e8647b2a27292b2f631efe2ac657afb2e2bb27c9
bb-in-hoodie <pso999@naver.com>
2026-05-05 16:52:37 +0900

    feat: inject metadata on build time

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+import {
+  BASE_URL,
+  SITE_DESCRIPTION,
+  SITE_NAME,
+} from "./src/common/constants/environment";
+
+const injectMetadata: Plugin = {
+  name: "inject-metadata",
+  transformIndexHtml: {
+    order: "pre",
+    handler(html) {
+      return html
+        .replace(/%SITE_NAME%/g, SITE_NAME)
+        .replace(/%SITE_DESCRIPTION%/g, SITE_DESCRIPTION)
+        .replace(/%BASE_URL%/g, BASE_URL);
     },
   },
+};
+
+export default defineConfig({
+  plugins: [react(), injectMetadata],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
