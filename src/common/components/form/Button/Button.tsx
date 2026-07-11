01925efb4f268595af62da61c15c7305a3f88d45
bb-in-hoodie <pso999@naver.com>
2026-07-11 16:21:38 +0900

    feat(design-system): Button component

---
+++ src/common/components/form/Button/Button.tsx
@@ -0,0 +1,37 @@
+import classNames from "classnames/bind";
+import { type ReactNode } from "react";
+
+import styles from "./Button.module.scss";
+
+const cx = classNames.bind(styles);
+
+export type ButtonProps = {
+  children: ReactNode;
+  onClick?: () => void;
+  variant?: "primary" | "secondary";
+  type?: "button" | "submit";
+  disabled?: boolean;
+  className?: string;
+};
+
+function Button({
+  children,
+  onClick,
+  variant = "secondary",
+  type = "button",
+  disabled,
+  className,
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={cx("button", variant, className)}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
